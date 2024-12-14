1f6147d38954b44daab382b4373aba12dd0e652e
Jkokol2000 <jameskokol2000@gmail.com>
2024-12-14 02:44:11 -0700

    Completely changed course, I think this is more fun anyway. We'll see how it goes

--- Restraunt_Crawling/Restraunt_Crawling.py
+++ Restraunt_Crawling/Restraunt_Crawling.py
@@ -1,15 +1,61 @@
+import requests
+from bs4 import BeautifulSoup
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import Select
+from csv import writer
 from time import sleep
-import csv
+from random import choice
+import re
+import numpy as np
 
-GameUrl = 'https://www.gamestop.com/stores/'
-driver = webdriver.Chrome()
-driver.get(GameUrl)
-cityEnter = driver.find_element("id", "store-search-input")
-cityEnter.send_keys('Atlanta, GA')
-submitBtn = driver.find_element(By.CLASS_NAME, "btn-storelocator-search")
-submitBtn.click()
-sleep(30)
+dr = webdriver.Chrome()
+
+all_titles = []
+
+all_names = ["Michael", "Percival_(Grand)", "Wamdus_(Holiday)", "Zeta_(Grand)", "Gabriel", "Payila", "Olivia", "Raziel_(Summer)", "Uriel", "Galleon_(Summer)", "Narmaya_(Grand)", "Vania_(Yukata)", "Nehan", "Sandalphon", "Ilsa_(Yukata)"]
+
+base_url = "https://gbf.wiki"
+
+url = '/Arulumaya'
+
+while url:
+    dr.get(f"{base_url}{url}")
+    print(f"Now Scraping{base_url}{url}")
+    soup = BeautifulSoup(dr.page_source, "html.parser")
+    current_title = soup.find(class_="char-title")
+    current_title_in_brackets = re.findall(r"\[(.*?)\]", current_title.get_text())
+    print(current_title_in_brackets)
+    all_titles.append({
+        "name" : "".join(ch for ch in url if ch.isalnum()),
+        "title": current_title_in_brackets
+        })
+    if all_names:
+        new_name = choice(all_names)
+        all_names.remove(new_name)
+        url = "/" + new_name
+        sleep(2)
+    else:
+        url = None
+selected_title = choice(all_titles)
+remaining_guesses = 3
+print("This title is: ")
+print(selected_title['title'])
+
+guess = ''
+while guess.lower() != selected_title["name"].lower() and remaining_guesses > 0:
+    guess = input(
+        f"Who's title is this? Guesses remaining {remaining_guesses}"
+    )
+
+    if guess == selected_title["name"]:
+        print("CORRECT")
+        break
+    remaining_guesses -= 1
+
+    if remaining_guesses == 3:
+        print(f"Nope, {remaining_guesses} guesses left")
+    elif remaining_guesses == 2:
+        print(f"Nope, {remaining_guesses} guesses left")
+    elif remaining_guesses == 1:
+        print(f"Nope, {remaining_guesses} guesses left")
+    else:
+        print(f"Sorry, out of guesses. The character with this title is {selected_title['name']}")
